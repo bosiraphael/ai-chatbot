4050188b45d17238fb6b21aaaab6e30cc72388af
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2023-06-26 11:38:01 +0200

    created api route

---
+++ app/api/route.ts
@@ -0,0 +1,23 @@
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { Configuration, OpenAIApi } from "openai-edge";
+
+const OPENAI_KEY = process.env.OPENAI_KEY;
+
+const config = new Configuration({
+  apiKey: OPENAI_KEY,
+});
+const openai = new OpenAIApi(config);
+
+export async function POST(request: Request) {
+  const { messages } = await request.json();
+
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages,
+  });
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
